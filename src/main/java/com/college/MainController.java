67a3d8ca4ddbbff463f730ce9b28b707bbc1271e
Deepak <reachdeepakgowda@gmail.com>
2018-06-11 12:25:02 +0530

    Change of file name

--- src/main/java/com/college/MainController.java
+++ src/main/java/com/college/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EasyNotesApplication {
+public class MainController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyNotesApplication.class, args);
