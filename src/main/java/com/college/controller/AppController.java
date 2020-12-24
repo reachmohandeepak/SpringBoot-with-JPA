9a8f16f5560218b97b52988e8fc4575b59ae7ff3
reachdeepakgowda@gmail.com <reachdeepakgowda@gmail.com>
2020-12-24 11:27:06 +0530

    Initial push

--- src/main/java/com/college/controller/AppController.java
+++ src/main/java/com/college/controller/AppController.java
@@ -1,17 +1,19 @@
 package com.college.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AppController {
 
-	@GetMapping(value = "/")
-	public String index() {
-		return "index";
-	}
-	@GetMapping(value = "/allStudentDeatails")
-	public String allStudentDeatails() {
-		return "student"; 
-	}
+    @GetMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(value = "/test")
+    public String allStudentDeatails() {
+        return "coupon";
+    }
 }
